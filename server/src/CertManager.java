7ec193adf09118796b9e1a33cc7595f4b62688fd
William <21342121@student.uwa.edu.au>
2016-05-14 13:49:01 +0800

    added todo outlines for certmanager class (subject to change)

--- server/src/CertManager.java
+++ server/src/CertManager.java
@@ -1,4 +1,21 @@
 
 public class CertManager {
+	//class for handling all certificate and trust matters
+	public CertManager(){
+		
+	}
+	
+	public void handshake(){
+		//process for handshaking
+	}
+	
+	public void vouch(String filename, String certname){
+		//code for vouching for a file on server
+	}
+	
+	public void determineLength(String certname){
+		//determining the length of a circle of trust -c		
+	}
 
+	
 }
